c06628be1e1c9719a405dd33a6e366e99520b152
RogerWillis <roger.willis@r3cev.com>
2018-08-07 17:27:21 +0100

    * Removed old tests.

--- src/test/kotlin/com/r3/testutils/TestConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.r3.testutils
-
-import com.r3.cash.issuer.common.types.AccountNumber
-import com.r3.cash.issuer.common.types.SortCode
-
-
-val ALICE_SORT_CODE = SortCode("012456")
-val ALICE_ACCOUNT_NUMBER = AccountNumber("24681012")
