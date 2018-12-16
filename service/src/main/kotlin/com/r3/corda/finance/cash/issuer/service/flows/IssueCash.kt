5518342d43edf624cee163fa87b673e50b317afc
Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>
2018-12-16 03:11:21 +0100

    use CashIssueAndPaymentFlow from finance module

--- service/src/main/kotlin/com/r3/corda/finance/cash/issuer/service/flows/IssueCash.kt
+++ service/src/main/kotlin/com/r3/corda/finance/cash/issuer/service/flows/IssueCash.kt
@@ -5,18 +5,20 @@ import com.r3.corda.finance.cash.issuer.common.contracts.NodeTransactionContract
 import com.r3.corda.finance.cash.issuer.common.states.NodeTransactionState
 import com.r3.corda.finance.cash.issuer.common.types.NodeTransactionStatus
 import net.corda.core.contracts.Amount
-import net.corda.core.flows.FinalityFlow
-import net.corda.core.flows.FlowLogic
-import net.corda.core.flows.FlowSession
-import net.corda.core.flows.StartableByService
+import net.corda.core.flows.*
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.OpaqueBytes
+import net.corda.finance.flows.CashIssueAndPaymentFlow
+import net.corda.finance.flows.CashIssueFlow
+
 
 // TODO: Denominations? E.g. 100 split between 10 issuances of 10.
 // TODO: Ask the counterparty for a random key and issue to that key.
 // TODO: Move all tx generation stuff to the common library.
 // TODO: Add the option for issuing to anonymous public keys.
 
+@InitiatingFlow
 @StartableByService
 class IssueCash(val stx: SignedTransaction) : FlowLogic<Pair<SignedTransaction, SignedTransaction>>() {
 
@@ -38,11 +40,12 @@ class IssueCash(val stx: SignedTransaction) : FlowLogic<Pair<SignedTransaction, 
         val internalFtx = subFlow(FinalityFlow(signedTransaction, emptySet<FlowSession>()))
 
         /** Commit the cash issuance transaction. */
-        val externalFtx = subFlow(IssueCashInternal(
-                to = nodeTransactionState.amountTransfer.destination,
-                amount = Amount(nodeTransactionState.amountTransfer.quantityDelta, nodeTransactionState.amountTransfer.token)
-        ))
-        return Pair(internalFtx, externalFtx)
+        val recipient = nodeTransactionState.amountTransfer.destination
+        val amount = Amount(nodeTransactionState.amountTransfer.quantityDelta, nodeTransactionState.amountTransfer.token)
+
+        val externalFtx = subFlow(CashIssueAndPaymentFlow(amount, OpaqueBytes.of(0), recipient, false, notary))
+
+        return Pair(internalFtx, externalFtx.stx)
     }
 
 }
