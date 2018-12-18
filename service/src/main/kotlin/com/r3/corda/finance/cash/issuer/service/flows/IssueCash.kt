c930fb448e62777bb0fd1445c3389db383333cee
Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>
2018-12-18 11:55:18 +0100

    CashIssueInternal works too, generateCash direct for receiver

--- service/src/main/kotlin/com/r3/corda/finance/cash/issuer/service/flows/IssueCash.kt
+++ service/src/main/kotlin/com/r3/corda/finance/cash/issuer/service/flows/IssueCash.kt
@@ -9,6 +9,8 @@ import net.corda.core.flows.*
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.OpaqueBytes
+import net.corda.core.utilities.ProgressTracker
+import net.corda.finance.flows.AbstractCashFlow
 import net.corda.finance.flows.CashIssueAndPaymentFlow
 import net.corda.finance.flows.CashIssueFlow
 
@@ -21,31 +23,55 @@ import net.corda.finance.flows.CashIssueFlow
 @InitiatingFlow
 @StartableByService
 class IssueCash(val stx: SignedTransaction) : FlowLogic<Pair<SignedTransaction, SignedTransaction>>() {
+    companion object {
+        object GENERATING_TX : ProgressTracker.Step("Generating node transaction")
+        object SIGNING_TX : ProgressTracker.Step("Signing node transaction")
+        object FINALISING_TX : ProgressTracker.Step("Obtaining notary signature and recording node transaction") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+        object ISSUE_CASH : ProgressTracker.Step("Issue cash internal") {
+            override fun childProgressTracker() = AbstractCashFlow.tracker()
+        }
 
+
+        @JvmStatic
+        fun tracker() = ProgressTracker(
+                GENERATING_TX,
+                SIGNING_TX,
+                FINALISING_TX,
+                ISSUE_CASH
+        )
+    }
+
+    override val progressTracker = tracker()
     @Suspendable
     override fun call(): Pair<SignedTransaction, SignedTransaction> {
         // Our chosen notary.
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
-
         val nodeTransactionStateAndRef = stx.tx.outRefsOfType<NodeTransactionState>().single()
         val nodeTransactionState = nodeTransactionStateAndRef.state.data
 
+        progressTracker.currentStep = GENERATING_TX
         // TODO: Check that the bank account states are verified.
         val internalBuilder = TransactionBuilder(notary = notary)
                 .addCommand(NodeTransactionContract.Update(), listOf(ourIdentity.owningKey))
                 .addInputState(nodeTransactionStateAndRef)
                 .addOutputState(nodeTransactionState.copy(status = NodeTransactionStatus.COMPLETE), NodeTransactionContract.CONTRACT_ID)
 
+        progressTracker.currentStep = SIGNING_TX
         val signedTransaction = serviceHub.signInitialTransaction(internalBuilder)
-        val internalFtx = subFlow(FinalityFlow(signedTransaction, emptySet<FlowSession>()))
+
+        progressTracker.currentStep = FINALISING_TX
+        val internalFtx = subFlow(FinalityFlow(signedTransaction, emptySet<FlowSession>(), FINALISING_TX.childProgressTracker()))
 
         /** Commit the cash issuance transaction. */
+        progressTracker.currentStep = ISSUE_CASH
         val recipient = nodeTransactionState.amountTransfer.destination
         val amount = Amount(nodeTransactionState.amountTransfer.quantityDelta, nodeTransactionState.amountTransfer.token)
+        //val externalFtx = subFlow(CashIssueAndPaymentFlow(amount, OpaqueBytes.of(0), recipient, false, notary))
+        val externalRes = subFlow(IssueCashInternal(recipient, amount, ISSUE_CASH.childProgressTracker()))
 
-        val externalFtx = subFlow(CashIssueAndPaymentFlow(amount, OpaqueBytes.of(0), recipient, false, notary))
-
-        return Pair(internalFtx, externalFtx.stx)
+        return Pair(internalFtx, externalRes.stx)
     }
 
 }
