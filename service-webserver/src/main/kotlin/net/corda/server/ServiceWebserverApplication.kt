4bad19da1a516c00958d19eb6310a71d9891c803
Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>
2019-01-03 12:38:18 +0100

    integration with swagger

--- service-webserver/src/main/kotlin/net/corda/server/ServiceWebserverApplication.kt
+++ service-webserver/src/main/kotlin/net/corda/server/ServiceWebserverApplication.kt
@@ -3,6 +3,7 @@ package net.corda.server
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -13,33 +14,31 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy
 import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = [
+	"net.corda.server",
+	"com.r3.corda.finance.cash.issuer.service.api.controllers"
+])
+@EnableSwagger2
 class Starter {
 	// When false, Tomcat is used
 	companion object {
 		val USE_JETTY = false
 	}
 
-	/** Registering rest controller */
-	@EnableWebMvc
-	@Configuration
-	@ComponentScan(basePackages = [
-		"net.corda.server.controllers",
-		"com.r3.corda.finance.cash.issuer.service.api.controllers"
-	])
-	open class WebConfig : WebMvcConfigurer
-
-
 	/** Registers an endpoint for STOMP messages. */
 	@Configuration
 	/** Registers an endpoint for STOMP messages. */
 	@EnableWebSocketMessageBroker
-	@ComponentScan(basePackages = [
-		"net.corda.server.controllers",
-		"com.r3.corda.finance.cash.issuer.service.api.controllers"
-	])
 	open class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 		override fun registerStompEndpoints(registry: StompEndpointRegistry) {
 			registry.addEndpoint("/stomp").setHandshakeHandler(handshakeHandler()).withSockJS()
@@ -63,6 +62,19 @@ class Starter {
 			registry.enableSimpleBroker("/topic", "/queue")
 		}
 */
+	}
+
+	@Configuration
+	@EnableSwagger2
+	inner class SwaggerConfig {
+		@Bean
+		fun api(): Docket {
+			return Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build()
+		}
 	}
 
 }
